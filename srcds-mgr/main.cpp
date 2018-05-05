dda9b8b88c2a5f5c42bdc6c18d81d5ef3f0e330d
Kenny McCormick <keennymccormic+github@gmail.com>
2018-05-05 17:52:28 -0500

    introduce graphics
    with sfml

--- srcds-mgr/main.cpp
+++ srcds-mgr/main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char* argv[])
 {
-	std::cout << "hello, world!" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SRCDS-Manager");
+
+	sf::Font arial;
+	arial.loadFromFile("C:/Windows/Fonts/Arial.ttf");
+
+	sf::Text helloWorldText;
+	helloWorldText.setFont(arial);
+	helloWorldText.setString("Hello, world!");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::EventType::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+
+		window.draw(helloWorldText);
+
+		window.display();
+	}
 }
