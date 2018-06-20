e95c1b721b5e677a874491980d4d390cf1501c83
Kenny McCormick <keennymccormic+github@gmail.com>
2018-06-20 13:34:37 -0500

    add SFUI & resources

--- srcds-mgr/main.cpp
+++ srcds-mgr/main.cpp
@@ -1,31 +1,93 @@
-#include <SFML/Graphics.hpp>
+#include <SFUI/Layouts/Menu.hpp>
+#include <SFUI/Theme.hpp>
+#include <SFUI/SFUI.hpp>
 
-int main(int argc, char* argv[])
+#include <iostream>
+
+int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SRCDS-Manager");
+	enum Callback
+	{
+		C_ITEM_NAME,
+		C_ITEM_DESCRIPTION,
+		C_ITEM_VERSION,
+		C_ITEM_RELEASE,
+		C_ITEM_FILES,
+		C_ITEM_ICON,
+		Submit,
+		Quit
+	};
 
-	sf::Font arial;
-	arial.loadFromFile("C:/Windows/Fonts/Arial.ttf");
+	// Create the main window
+	sf::RenderWindow app(sf::VideoMode(400, 400), "Item Upload v1");
 
-	sf::Text helloWorldText;
-	helloWorldText.setFont(arial);
-	helloWorldText.setString("Hello, world!");
+	SFUI::Menu menu(app);
+	menu.setPosition(sf::Vector2f(10, 10));
 
-	while (window.isOpen())
+	SFUI::Theme::loadFont("resources/interface/tahoma.ttf");
+	SFUI::Theme::loadTexture("resources/interface/texture_square.png");
+	SFUI::Theme::fontSize = 11;
+	SFUI::Theme::click.textColor = SFUI::Theme::hexToRgb("#191B18");
+	SFUI::Theme::click.textColorHover = SFUI::Theme::hexToRgb("#191B18");
+	SFUI::Theme::click.textColorFocus = SFUI::Theme::hexToRgb("#000");
+	SFUI::Theme::input.textColor = SFUI::Theme::hexToRgb("#000");
+	SFUI::Theme::input.textColorHover = SFUI::Theme::hexToRgb("#000");
+	SFUI::Theme::input.textColorFocus = SFUI::Theme::hexToRgb("#000");
+	SFUI::Theme::PADDING = 2.f;
+	SFUI::Theme::windowBgColor = SFUI::Theme::hexToRgb("#dddbde");
+
+	SFUI::HorizontalBoxLayout* hbox = menu.addHorizontalBoxLayout();
+	SFUI::FormLayout* form = hbox->addFormLayout();
+
+	form->addLabel("Input item info");
+
+	SFUI::InputBox* textureName = new SFUI::InputBox();
+	textureName->setText("resources/texture_");
+	form->addRow("Texture", textureName, C_ITEM_NAME);
+
+	SFUI::InputBox* itemName = new SFUI::InputBox();
+	form->addRow("Item Name", itemName, C_ITEM_NAME);
+
+	SFUI::InputBox* itemDescription = new SFUI::InputBox();
+	form->addRow("Item Description", itemDescription, C_ITEM_DESCRIPTION);
+
+	SFUI::InputBox* itemVersion = new SFUI::InputBox();
+	form->addRow("Item Name", itemVersion, C_ITEM_VERSION);
+
+	SFUI::InputBox* itemIcon = new SFUI::InputBox();
+	itemIcon->setText("./files/icon.png");
+	form->addRow("Item Name", itemIcon, C_ITEM_ICON);
+
+	form->addButton("Submit", Submit);
+
+	while (app.isOpen())
 	{
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (app.pollEvent(event))
 		{
-			if (event.type == sf::Event::EventType::Closed)
+			int id = menu.onEvent(event);
+			switch (id)
 			{
-				window.close();
+			case Submit:
+				std::cout << "doing the do" << std::endl;
+
+				SFUI::Theme::loadTexture(textureName->getText());
+
+				break;
+			case Quit:
+				app.close();
 			}
+
+			if (event.type == sf::Event::Closed)
+				app.close();
 		}
 
-		window.clear();
+		app.clear(SFUI::Theme::windowBgColor);
 
-		window.draw(helloWorldText);
+		app.draw(menu);
 
-		window.display();
+		app.display();
 	}
+
+	return EXIT_SUCCESS;
 }
